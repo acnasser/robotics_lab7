87cd172900123a3f386611deec1779a76dd544b4
ALaws0n <andrewclawson01@gmail.com>
2024-04-19 11:48:45 -0400

    Fixes for lab 7 testing - ACL

--- scripts/final_planner.py
+++ scripts/final_planner.py
@@ -15,6 +15,7 @@ points_in_camera = []
 first_point = []
 counter = 0
 
+
 def xyzCallback(data):
     global points_in_camera
     x = data.xc
@@ -36,8 +37,10 @@ def tpCallback(data):
         counter += 1
 
 def main():
+
     rospy.init_node('simple_planner', anonymous=True)
     plan_pub = rospy.Publisher('/plan', Plan, queue_size=10)
+
    
     rospy.Subscriber('sphere_params', SphereParams, xyzCallback)
     rospy.Subscriber('/ur5e/toolpose', Twist, tpCallback)
@@ -57,15 +60,15 @@ def main():
             plan_point1.linear.x = first_point[0]
             plan_point1.linear.y = first_point[1] 
             plan_point1.linear.z = first_point[2]
-            plan_point1.angular.x = 3.09069
-            plan_point1.angular.y = 0.06071
-            plan_point1.angular.z = 1.56788
+            plan_point1.angular.x = first_point[3]
+            plan_point1.angular.y = first_point[4]
+            plan_point1.angular.z = first_point[5]
             point_mode1.data = 0
             
             plan.points.append(plan_point1)
             plan.modes.append(point_mode1)
             
-			#--------------------------------------------------------------------
+	    #--------------------------------------------------------------------
            
             pt_in_cam = PointStamped()
             pt_in_cam.header.frame_id = 'camera_color_optical_frame'
@@ -75,73 +78,134 @@ def main():
             pt_in_cam.point.z = points_in_camera[2]
             points_in_base = tfBuffer.transform(pt_in_cam, 'base', rospy.Duration(1.0))
    			
-   			#--------------------------------------------------------------------
+   	    #--------------------------------------------------------------------
            
-            plan_point2 = Twist()
-            point_mode2 = UInt8()
+            plan_point_to_ball = Twist()
+            point_mode_to_ball = UInt8()
             
-            plan_point2.linear.x = points_in_base.point.x + 0.1
-            plan_point2.linear.y = points_in_base.point.y
-            plan_point2.linear.z = points_in_base.point.z
-            plan_point2.angular.x = 3.09069
-            plan_point2.angular.y = 0.06071
-            plan_point2.angular.z = 1.56788
-            point_mode2.data = 1
+            plan_point_to_ball.linear.x = points_in_base.point.x 
+            plan_point_to_ball.linear.y = points_in_base.point.y
+            plan_point_to_ball.linear.z = points_in_base.point.z + 0.01
+            plan_point_to_ball.angular.x = first_point[3]
+            plan_point_to_ball.angular.y = first_point[4]
+            plan_point_to_ball.angular.z = first_point[5]
+            point_mode_to_ball.data = 0
             
-            plan.points.append(plan_point2)
-            plan.modes.append(point_mode2)
-            
-            #---------------------------------------------------------------------
-            
-            plan_point3 = Twist()
-            point_mode3 = UInt8()
-            
-            plan_point3.linear.x = points_in_base.point.x + 0.06
-            plan_point3.linear.y = points_in_base.point.y
-            plan_point3.linear.z = points_in_base.point.z
-            plan_point3.angular.x = 3.09069
-            plan_point3.angular.y = 0.06071
-            plan_point3.angular.z = 1.56788
-            point_mode3.data = 2
-            
-            plan.points.append(plan_point2)
-            plan.modes.append(point_mode3)
+            plan.points.append(plan_point_to_ball)
+            plan.modes.append(point_mode_to_ball)
             
             #--------------------------------------------------------------------
-           
-            plan_point4 = Twist()
-            point_mode4 = UInt8()
             
-            plan_point4.linear.x = -0.5
-            plan_point4.linear.y = .3
-            plan_point4.linear.z = 0.4
-            plan_point4.angular.x = 3.09069
-            plan_point4.angular.y = 0.06071
-            plan_point4.angular.z = 1.56788
-            point_mode4.data = 2
+            plan_point_close_grip = Twist()
+            point_mode_close_grip = UInt8()
             
-            plan.points.append(plan_point4)
-            plan.modes.append(point_mode4)
+            plan_point_close_grip.linear.x = points_in_base.point.x 
+            plan_point_close_grip.linear.y = points_in_base.point.y
+            plan_point_close_grip.linear.z = points_in_base.point.z + 0.01
+            plan_point_close_grip.angular.x = first_point[3]
+            plan_point_close_grip.angular.y = first_point[4]
+            plan_point_close_grip.angular.z = first_point[5]
+            point_mode_close_grip.data = 2
             
-            #---------------------------------------------------------------------
-   
-            plan_point5 = Twist()
-            point_mode5 = UInt8()
-            
-            plan_point5.linear.x = -0.5
-            plan_point5.linear.y = .3
-            plan_point5.linear.z = 0.2
-            plan_point5.angular.x = 3.09069
-            plan_point5.angular.y = 0.06071
-            plan_point5.angular.z = 1.56788
-            point_mode5.data = 1
-            
-            plan.points.append(plan_point5)
-            plan.modes.append(point_mode5)
+            plan.points.append(plan_point_close_grip)
+            plan.modes.append(point_mode_close_grip)
             
             #---------------------------------------------------------------------
+            
+            plan_point_raise_gripper = Twist()
+            point_mode_raise_gripper = UInt8()
+            
+            plan_point_raise_gripper.linear.x = points_in_base.point.x 
+            plan_point_raise_gripper.linear.y = points_in_base.point.y
+            plan_point_raise_gripper.linear.z = points_in_base.point.z + 0.05
+            plan_point_raise_gripper.angular.x = first_point[3]
+            plan_point_raise_gripper.angular.y = first_point[4]
+            plan_point_raise_gripper.angular.z = first_point[5]
+            point_mode_raise_gripper.data = 0
+            
+            plan.points.append(plan_point_raise_gripper)
+            plan.modes.append(point_mode_raise_gripper)
+            
+            #--------------------------------------------------------------------
+            
+            plan_point_move_fwrd = Twist()
+            point_mode_move_fwrd = UInt8()
+            
+            plan_point_move_fwrd.linear.x = points_in_base.point.x 
+            plan_point_move_fwrd.linear.y = points_in_base.point.y - 0.05
+            plan_point_move_fwrd.linear.z = points_in_base.point.z + 0.05
+            plan_point_move_fwrd.angular.x = first_point[3]
+            plan_point_move_fwrd.angular.y = first_point[4]
+            plan_point_move_fwrd.angular.z = first_point[5]
+            point_mode_move_fwrd.data = 0
+            
+            plan.points.append(plan_point_move_fwrd)
+            plan.modes.append(point_mode_move_fwrd)
+            
+            #--------------------------------------------------------------------
+            
+            plan_point_move_down = Twist()
+            point_mode_move_down = UInt8()
+            
+            plan_point_move_down.linear.x = points_in_base.point.x 
+            plan_point_move_down.linear.y = points_in_base.point.y - 0.05
+            plan_point_move_down.linear.z = points_in_base.point.z + 0.01
+            plan_point_move_down.angular.x = first_point[3]
+            plan_point_move_down.angular.y = first_point[4]
+            plan_point_move_down.angular.z = first_point[5]
+            point_mode_move_down.data = 0
+            
+            plan.points.append(plan_point_move_down)
+            plan.modes.append(point_mode_move_down)
+            
+            #--------------------------------------------------------------------
+            
+            plan_point_drop_ball = Twist()
+            point_mode_drop_ball = UInt8()
+            
+            plan_point_drop_ball.linear.x = points_in_base.point.x 
+            plan_point_drop_ball.linear.y = points_in_base.point.y - 0.05
+            plan_point_drop_ball.linear.z = points_in_base.point.z + 0.01
+            plan_point_drop_ball.angular.x = first_point[3]
+            plan_point_drop_ball.angular.y = first_point[4]
+            plan_point_drop_ball.angular.z = first_point[5]
+            point_mode_drop_ball.data = 1
+            
+            plan.points.append(plan_point_drop_ball)
+            plan.modes.append(point_mode_drop_ball)
+            
+            #--------------------------------------------------------------------
+            
+            plan_point_move_up = Twist()
+            point_mode_move_up = UInt8()
+            
+            plan_point_move_up.linear.x = points_in_base.point.x 
+            plan_point_move_up.linear.y = points_in_base.point.y - 0.05
+            plan_point_move_up.linear.z = points_in_base.point.z + 0.05
+            plan_point_move_up.angular.x = first_point[3]
+            plan_point_move_up.angular.y = first_point[4]
+            plan_point_move_up.angular.z = first_point[5]
+            point_mode_move_up.data = 0
+            
+            plan.points.append(plan_point_move_up)
+            plan.modes.append(point_mode_move_up)
+            
 
-            plan_pub.publish(plan)
+          
+            #--------------------------------------------------------------------
+			
+            
+            move = input("Would you like to move the robot? y/n")
+            
+            if move == "y":
+            	plan_pub.publish(plan)
+            else:
+            	print(plan)
+            
+            
+            #---------------------------------------------------------------------
+		
+           
             loop_rate.sleep()
 
 if __name__ == '__main__':
